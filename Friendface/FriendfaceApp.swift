7a925646539495ba808d5e93e311df7adf14fab9
Dom Bryan <dombguitar@gmail.com>
2022-02-10 13:46:30 +0000

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Dom Bryan on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
